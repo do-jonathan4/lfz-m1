a0c48ad7cece427bde8f5e00ca39c298e6823c44
Uzair Ashraf <uzinatorcl@live.com>
2020-10-06 11:53:20 -0700

    added data modeling 2

---
+++ exercises/data-modeling-2/main.js
@@ -0,0 +1,37 @@
+var products = [
+  {
+    name: 'JavaScript T-Shirt',
+    category: 'Men \'s Apparel',
+    price: 39,
+    imageUrl: 'https://m.media-amazon.com/images/I/A13usaonutL._AC_CLa%7C2140%2C2000%7C81DBK0a0FVL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UL1500_.png',
+    sizes: [
+      's',
+      'm',
+      'l',
+      'xl'
+    ],
+    colors: [
+      'black',
+      'red',
+      'blue',
+      'green'
+    ]
+  },
+  {
+    name: 'Node.js T-Shirt',
+    category: 'Men \'s Apparel',
+    price: 35,
+    imageUrl: 'https://cdn.designbyhumans.com/product_images/p/1084894.65.f68a3S7ayBAA-650x650-b-p-eee.jpg',
+    sizes: [
+      's',
+      'm',
+      'l',
+    ],
+    colors: [
+      'white',
+      'blue',
+      'purple',
+      'green'
+    ]
+  }
+]
